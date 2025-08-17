081ab01dbb94638da472fae9a9ce81a461885e0d
camlin-tan <187715771+camlin-tan@users.noreply.github.com>
2025-08-17 13:38:27 +0800

    add support for list

--- src/main/java/Bruh.java
+++ src/main/java/Bruh.java
@@ -1,20 +1,24 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Bruh {
+    private static final String LINE = "  ____________________________________________________________\r\n   ";
 
     public static void echo() {
         Scanner scnr = new Scanner(System.in);
         String userInput = scnr.nextLine();
         while (!userInput.equalsIgnoreCase("bye")) {
-            String response = "  ____________________________________________________________\r\n   "
+            String response = LINE
                     + userInput + "\r\n" +
-                    "  ____________________________________________________________\r\n";
+                    LINE;
             System.out.println(response);
             userInput = scnr.nextLine();
         }
     }
 
     public static void main(String[] args) {
+        ArrayList<String> listStrings = new ArrayList<>();
+        boolean running = true;
         String greeting = "  ____________________________________________________________\r\n" +
                 "   Hello! I'm Bruh\r\n" +
                 "   What can I do for you?\r\n" +
@@ -22,8 +26,32 @@ public class Bruh {
         String farewell = "  ____________________________________________________________\r\n" +
                 "   Bye. Hope to see you again soon!\r\n" +
                 "  ____________________________________________________________\r\n";
+        Scanner scnr = new Scanner(System.in);
+
         System.out.println(greeting);
-        echo();
+        while (running) {
+            String userInput = scnr.nextLine();
+            switch (userInput.toLowerCase()) {
+                case "bye":
+                    running = false;
+                    break;
+                case "list":
+                    String itemsString = "";
+                    for (int i = 0; i < listStrings.size(); i++) {
+                        itemsString += ((i + 1) + ". " + listStrings.get(i) + "\r\n   ");
+                    }
+                    System.out.println(LINE +
+                            itemsString.trim() + "\r\n" +
+                            LINE);
+                    break;
+                default:
+                    String response = LINE +
+                            "added: " + userInput + "\r\n" +
+                            LINE;
+                    listStrings.add(userInput);
+                    System.out.println(response);
+            }
+        }
         System.out.println(farewell);
     }
 }
