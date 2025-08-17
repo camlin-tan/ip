20b679904c289a2e0b50963ab29c751e529d5253
camlin-tan <187715771+camlin-tan@users.noreply.github.com>
2025-08-17 13:03:06 +0800

    Added support for echo

--- src/main/java/Bruh.java
+++ src/main/java/Bruh.java
@@ -1,11 +1,29 @@
+import java.util.Scanner;
+
 public class Bruh {
+
+    public static void echo() {
+        Scanner scnr = new Scanner(System.in);
+        String userInput = scnr.nextLine();
+        while (!userInput.equalsIgnoreCase("bye")) {
+            String response = "  ____________________________________________________________\r\n   "
+                    + userInput + "\r\n" +
+                    "  ____________________________________________________________\r\n";
+            System.out.println(response);
+            userInput = scnr.nextLine();
+        }
+    }
+
     public static void main(String[] args) {
-        String greeting = "____________________________________________________________\r\n" +
-                " Hello! I'm Bruh\r\n" +
-                " What can I do for you?\r\n" +
-                "____________________________________________________________\r\n" +
-                " Bye. Hope to see you again soon!\r\n" +
-                "____________________________________________________________\r\n";
+        String greeting = "  ____________________________________________________________\r\n" +
+                "   Hello! I'm Bruh\r\n" +
+                "   What can I do for you?\r\n" +
+                "  ____________________________________________________________\r\n";
+        String farewell = "  ____________________________________________________________\r\n" +
+                "   Bye. Hope to see you again soon!\r\n" +
+                "  ____________________________________________________________\r\n";
         System.out.println(greeting);
+        echo();
+        System.out.println(farewell);
     }
 }
