7dcada657619433b463193ac60ccaaa7ea8f9c42
camlin-tan <187715771+camlin-tan@users.noreply.github.com>
2025-08-17 12:21:17 +0800

    Add greeting

--- src/main/java/Bruh.java
+++ src/main/java/Bruh.java
@@ -1,10 +1,11 @@
 public class Bruh {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String greeting = "____________________________________________________________\r\n" +
+                " Hello! I'm Bruh\r\n" +
+                " What can I do for you?\r\n" +
+                "____________________________________________________________\r\n" +
+                " Bye. Hope to see you again soon!\r\n" +
+                "____________________________________________________________\r\n";
+        System.out.println(greeting);
     }
 }
