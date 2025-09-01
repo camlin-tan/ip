2f274bc21d8a1df5c2f70df7d33bc3372b5839c0
camlin-tan <187715771+camlin-tan@users.noreply.github.com>
2025-09-01 20:51:51 +0800

    Update JavaDoc of Ui.java

--- src/main/java/bruh/ui/Ui.java
+++ src/main/java/bruh/ui/Ui.java
@@ -12,29 +12,56 @@ public class Ui {
     private static final String LINE = "  ____________________________________________________________\r\n   ";
     private Scanner scnr;
 
+    /**
+     * Default constructor for Ui
+     */
     public Ui() {
         this.scnr = new Scanner(System.in);
     }
 
+    /**
+     * Constructor for Ui with custom Scanner
+     * 
+     * @param scnr
+     */
     public Ui(Scanner scnr) {
         this.scnr = scnr;
     }
 
+    /**
+     * Show a horizontal line
+     */
     public void showLine() {
         System.out.println(LINE);
     }
 
+    /**
+     * Show welcome message
+     * 
+     * @return
+     */
     public String showWelcome() {
         System.out.println(LINE + "Hello! I'm Bruh");
         System.out.println("   What can I do for you?\r\n" + LINE);
         return LINE + "Hello! I'm Bruh   What can I do for you?\r\n" + LINE;
     }
 
+    /**
+     * Show farewell message
+     * 
+     * @return
+     */
     public String showFarewell() {
         System.out.println(LINE + "Bye. Hope to see you again soon!\r\n" + LINE);
         return LINE + "Bye. Hope to see you again soon!\r\n" + LINE;
     }
 
+    /**
+     * List all tasks
+     * 
+     * @param tasks
+     * @return
+     */
     public String listTasks(ArrayList<Task> tasks) {
         if (tasks.isEmpty()) {
             System.out.println(LINE + "No tasks in the list yet or for date specified.\r\n" + LINE);
@@ -49,21 +76,44 @@ public class Ui {
         }
     }
 
+    /**
+     * Show error message
+     * 
+     * @param message
+     * @return
+     */
     public String showError(String message) {
         System.out.println(LINE + message + "\r\n" + LINE);
         return LINE + message + "\r\n" + LINE;
     }
 
+    /**
+     * Show loading error message
+     * 
+     * @param message
+     * @return
+     */
     public String showLoadingError(String message) {
         System.out.println("Error loading tasks from hard disk: " + message);
         return "Error loading tasks from hard disk: " + message;
     }
 
+    /**
+     * Show a generic message
+     * 
+     * @param message
+     * @return
+     */
     public String showMessage(String message) {
         System.out.println(LINE + message + "\r\n" + LINE);
         return LINE + message + "\r\n" + LINE;
     }
 
+    /**
+     * Read user command
+     * 
+     * @return
+     */
     public String readCommand() {
         // System.out.print("Enter command: ");
         return scnr.nextLine();
